a3226d9a0a8002ddafe7417801050001b62d7817
quentin452 <42176772+quentin452@users.noreply.github.com>
2025-01-05 17:51:25 +0000

    Avoid redudancy in MixinItemStack

--- src/main/java/mixac1/dangerrpg/mixins/common/vanilla/ItemSystem/MixinItemStack.java
+++ src/main/java/mixac1/dangerrpg/mixins/common/vanilla/ItemSystem/MixinItemStack.java
@@ -10,14 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemStack.class)
 public class MixinItemStack {
-    @Inject(method = "<init>", at = @At("RETURN"))
-    public void initialize(CallbackInfo ci) {
-        ItemStack stack = (ItemStack) (Object) this;
-        if (RPGItemHelper.isRPGable(stack)) {
-            RPGItemHelper.initRPGItem(stack);
-        }
-    }
-
     @Inject(method = "readFromNBT", at = @At("RETURN"))
     public void readFromNBT(NBTTagCompound nbt, CallbackInfo ci) {
         ItemStack stack = (ItemStack) (Object) this;
